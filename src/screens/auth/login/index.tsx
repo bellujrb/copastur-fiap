bae393d562488db7b3a06d4998439a551b247a34
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-05-07 12:10:39 -0300

    copastur-fiap

--- src/screens/auth/login/index.tsx
+++ src/screens/auth/login/index.tsx
@@ -64,7 +64,7 @@ export default function Login(){
 
                 { /* Button Register */ }
                     <Button nameButton={loginbutton} destiny={() => {
-                        LoginAccount()
+                        nav.navigate('home')
                     }}/>
                 { /* End Button Register */ }
 
--- src/screens/auth/register/index.tsx
+++ src/screens/auth/register/index.tsx
@@ -80,7 +80,7 @@ export default function Register(){
 
                 { /* Button Register */ }
                     <Button nameButton={confirmregister} destiny={() => {
-                        CreateAccount()
+                        nav.navigate('home')
                     }}/>
                 { /* End Button Register */ }
 
